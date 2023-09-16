5626ab4246a70b580285e7fedae5d25edc4d219c
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-09-16 10:49:19 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "691445e8-2ee5-42f3-ba21-84f95226ef73",
+    "home_feed": [
+        {
+            "id": "8520553a-8f77-4345-b990-32043840823f",
+            "artist_name": "throw",
+            "home_carousel": [
+                {
+                    "id": "97f11cc5-a517-497d-b262-b9db2466b06a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/359.png"
+                },
+                {
+                    "id": "77d5a314-8cc6-4307-b966-11751ad929b5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png"
+                },
+                {
+                    "id": "0a91c3d5-9d90-4e10-9c24-5274714c8fee",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/53.png"
+                },
+                {
+                    "id": "083872aa-1a88-478f-8d87-5bcaad0cf06a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/106.png"
+                },
+                {
+                    "id": "0d372a0b-a733-47d6-9c9e-b9c2be9c0b08",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/278.png"
+                }
+            ],
+            "art_caption": "price",
+            "art_description": "Page cover bank return. Whose catch study arrive response huge though. Institution charge time least opportunity officer agreement."
+        },
+        {
+            "id": "ccd98f58-5897-4b0b-9b4c-a02c592db573",
+            "artist_name": "time",
+            "home_carousel": [
+                {
+                    "id": "2f7ff685-f0b3-4e66-9078-ab2b1c7ced25",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/252.png"
+                },
+                {
+                    "id": "da4817dc-21d2-4530-92d2-fd5a5d9c6b3c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/141.png"
+                },
+                {
+                    "id": "a1f3f562-ccfc-4f34-8d79-64831251c195",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/4.png"
+                },
+                {
+                    "id": "4f4a0430-6f55-4d2f-9b09-7e34cf076898",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/105.png"
+                },
+                {
+                    "id": "1a463814-9266-4141-aa36-ef9d33d2c86c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/126.png"
+                }
+            ],
+            "art_caption": "anyone",
+            "art_description": "Unit hand fund standard why bit. Believe attorney prove everything big ago idea. Federal maybe level worry. Never visit market."
+        },
+        {
+            "id": "728a0d2d-5dd4-4b44-bcc0-85e82d212771",
+            "artist_name": "claim",
+            "home_carousel": [
+                {
+                    "id": "2381ce76-8cea-4eea-8dbb-81eb8d2c5afb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/352.png"
+                },
+                {
+                    "id": "5a27d9c0-54f4-45be-8663-ec518869c2dd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/153.png"
+                },
+                {
+                    "id": "7c244776-212d-403a-aff3-8a8eb7bd779a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/5.png"
+                },
+                {
+                    "id": "dfd0eaba-8ef1-48ab-8320-3cc3c0dfa4d0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/338.png"
+                },
+                {
+                    "id": "94946375-b895-4641-baa2-8a4a63571b49",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/236.png"
+                }
+            ],
+            "art_caption": "which",
+            "art_description": "News song analysis citizen. Song best may."
+        },
+        {
+            "id": "e5716bc0-13b1-4c83-849f-b62dde380fcd",
+            "artist_name": "most",
+            "home_carousel": [
+                {
+                    "id": "515059d1-9d18-4499-89ee-afc310132992",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/209.png"
+                },
+                {
+                    "id": "85bf46c2-1a49-45c4-bcc0-5ea539cf0fed",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/96.png"
+                },
+                {
+                    "id": "3b8509b7-494a-41eb-a311-c6a10b5779b6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/243.png"
+                },
+                {
+                    "id": "07e94389-5f6b-4156-b306-1c76f4282bc3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/153.png"
+                },
+                {
+                    "id": "f06e91ed-3b86-4777-9328-575affa5d628",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png"
+                }
+            ],
+            "art_caption": "special",
+            "art_description": "Stay her son school member sort research. Open type leader green. Pay six have sound wind."
+        },
+        {
+            "id": "7e0a3090-1f9f-496c-950a-bb5adb208502",
+            "artist_name": "there",
+            "home_carousel": [
+                {
+                    "id": "ff6685f4-e07e-4100-b7a7-33318c78214b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/77.png"
+                },
+                {
+                    "id": "b118ad04-8a6a-4f68-8e8e-c7af29ae3aee",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/78.png"
+                },
+                {
+                    "id": "ca558839-cf9a-4a03-b369-e473d76380cd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/166.png"
+                },
+                {
+                    "id": "c53db16d-d6f9-4ae4-9753-57605fe7fd48",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/218.png"
+                },
+                {
+                    "id": "047ae96c-070e-41df-b846-c2f36a78e41b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png"
+                }
+            ],
+            "art_caption": "work",
+            "art_description": "Physical large north throw determine senior when. Parent check public college type customer low."
+        }
+    ]
+}
